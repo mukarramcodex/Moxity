eb88f68bc934c0372d7ca9884d80782d1cc186ea
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-23 12:06:50 +0500

    functions file add

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Theme Functions and Definations
+ * 
+ *
+ * @package MoxityElementor
+ * 
+ */
+
+if ( !defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+define( 'Moxity_Elementor_VERSION', '1.0.1');
+define( 'EHP_THEME_SLUG', 'moxity-elementor');
+
+define( 'MOXITY_THEME_PATH', get_template_directory());
+define( 'MOXITY_THEME_URL', get_template_directory_uri());
